ee4478605366f08c833e7ffa669c504504a6651a
Rhenios <renios420@icloud.com>
2023-01-17 10:15:15 +0900

    Initial commit

---
+++ src/main/java/org/Main/Main.java
@@ -0,0 +1,11 @@
+package org.Main;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException, InterruptedException {
+
+        JDA jda = JDABuilder.createDefault();
+
+    }
+}
