bb6efdb074859395952d150d27030c23c97aeb0c
Rhenios <renios420@icloud.com>
2023-01-17 10:31:04 +0900

    init

--- src/main/java/org/Main/Main.java
+++ src/main/java/org/Main/Main.java
@@ -1,11 +1,17 @@
 package org.Main;
 
+
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+
 import javax.security.auth.login.LoginException;
 
 public class Main {
+
+    DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault("");
+
     public static void main(String[] args) throws LoginException, InterruptedException {
 
-        JDA jda = JDABuilder.createDefault();
+
 
     }
 }
